8b6f6caf00398209b3c717a757f95a1d2029bcf5
bacancy-maitry <maitry.shah@bacnacytechnology.com>
2019-01-24 00:50:56 +0530

    Commit

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -13,13 +13,18 @@ export class ListUserComponent implements OnInit {
   waitingMessageDisplay = "Please Wait while we are getting user details...";
   waitingMessage: boolean = false;
 
+  displayUserList: boolean = true;
+
   fetchMessageDisplay = "Fetching Data...";
   fetchMessage: boolean = false;
 
   displayDataArray: Array<UserData> = [];
   allData: UserDataInterface;
 
-  totalPage:number;
+  totalPages:Array<number> = [];
+  i:number;
+
+  userForm: boolean = false;
 
   constructor(private http: HttpClient, private mainService: MainService) { }
 
@@ -28,10 +33,9 @@ export class ListUserComponent implements OnInit {
     this.fetchMessage = false; 
   }
 
-  
+  // ListUsers
   showUserList(pageNo: number) {
-    this.fetchMessage = true; 
-
+    this.fetchMessage = true;
     this.mainService.getUserList(pageNo).subscribe(response => {
       console.log("Page 1 Data::" , response);
       if(response && response.data){
@@ -41,16 +45,21 @@ export class ListUserComponent implements OnInit {
         this.displayDataArray = this.allData.data;
         console.log("Data Array:: ", this.displayDataArray);
 
-        this.totalPage = this.allData.total_pages;
-
+        for(this.i=0; this.i<this.allData.total_pages; this.i++){
+          this.totalPages[this.i] = this.i + 1;
+        }
         this.waitingMessage = true;
       }
       this.fetchMessage = false;
     });
+    this.displayUserList = true;
+    this.userForm = false;
   }
 
   addUserForm(){
     console.log("Add USer:::");
+    this.displayUserList = false;
+    this.userForm = true;
   }
 
 }
