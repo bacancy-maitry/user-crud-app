8fd3d9ed1b3e425b53c3299c04c55fab29b01d2e
bacancy-maitry <maitry.shah@bacnacytechnology.com>
2019-01-24 11:23:59 +0530

    Commit

--- src/app/list-user/list-user.component.ts
+++ src/app/list-user/list-user.component.ts
@@ -37,13 +37,13 @@ export class ListUserComponent implements OnInit {
   showUserList(pageNo: number) {
     this.fetchMessage = true;
     this.mainService.getUserList(pageNo).subscribe(response => {
-      console.log("Page 1 Data::" , response);
+      // console.log("Page 1 Data::" , response);
       if(response && response.data){
         this.allData = response;
-        console.log("All Data::" , this.allData);
+        // console.log("All Data::" , this.allData);
 
         this.displayDataArray = this.allData.data;
-        console.log("Data Array:: ", this.displayDataArray);
+        // console.log("Data Array:: ", this.displayDataArray);
 
         for(this.i=0; this.i<this.allData.total_pages; this.i++){
           this.totalPages[this.i] = this.i + 1;
@@ -57,7 +57,7 @@ export class ListUserComponent implements OnInit {
   }
 
   addUserForm(){
-    console.log("Add USer:::");
+    // console.log("Add USer:::");
     this.displayUserList = false;
     this.userForm = true;
   }
